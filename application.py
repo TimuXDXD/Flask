4b6a2bc6c28bbee3c317cf783de28943802a48cf
TimuXDXD <timu890518@gmail.com>
2022-03-18 10:40:05 +0800

    Update application.py

--- application.py
+++ application.py
@@ -1,22 +1,31 @@
 import flask
-from flask import request
-from flask import jsonify
+from flask import request, jsonify, redirect, url_for
 app = flask.Flask(__name__)
 
 @app.route("/")
 def index():
-    ip_addr = request.remote_addr
-    return '<h1> Your IP address is:' + ip_addr + '</h1>'
+    return redirect(url_for('ip'))
 
-@app.route('/c')
-def client():
-    ip_addr = request.environ['REMOTE_ADDR']
-    return '<h1> Your IP address is:' + ip_addr + '</h1>'
+@app.route('/ip')
+def ip():
+    template = ''
+    ip = getIP()
+    ip = [_ for _ in ip if not _.startswith('169.254')]
+    if ip:
+        template = '<h1> IP: ' + ip[0] + '</h1>'
+    return template
 
-@app.route('/pc')
-def proxy_client():
-    ip_addr = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
-    return '<h1> Your IP address is:' + ip_addr + '</h1>'
+def getIP():
+    def clientV1():
+        return request.remote_addr
+
+    def clientV2():
+        return request.environ['REMOTE_ADDR']
+
+    def proxy_client():
+        return request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
+
+    return [clientV1(), clientV2(), proxy_client()]
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5000, debug=True)
